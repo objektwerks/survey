7fcac0837ea7a80f385c57da5032d6af62b7bfff
objektwerks <objektwerks@runbox.com>
2025-05-21 12:15:13 -0400

    added account

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -1,4 +1,14 @@
 package objektwerks
 
+import java.util.UUID
+
 sealed trait Entity:
   val id: Long
+
+final case class Account(
+    id: Long = 0,
+    license: String = UUID.randomUUID.toString,
+    email: String = "",
+    pin: String = Pin.newInstance,
+    activated: String = Entity.now
+) extends Entity derives CanEqual
