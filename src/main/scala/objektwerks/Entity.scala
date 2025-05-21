955572fcd6597fb460a43639497ea5eec262bdda
objektwerks <objektwerks@runbox.com>
2025-05-21 12:16:56 -0400

    enhanced entity

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -1,14 +1,16 @@
 package objektwerks
 
+import java.time.LocalDate
 import java.util.UUID
 
 sealed trait Entity:
   val id: Long
+  def now(): String = LocalDate.now.toString
 
 final case class Account(
-    id: Long = 0,
-    license: String = UUID.randomUUID.toString,
-    email: String = "",
-    pin: String = Pin.newInstance,
-    activated: String = Entity.now
+  id: Long = 0,
+  license: String = UUID.randomUUID.toString,
+  email: String = "",
+  pin: String = Pin.newInstance,
+  activated: String = now()
 ) extends Entity derives CanEqual
