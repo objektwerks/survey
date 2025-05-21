8112804585a856b08ff1671731c1849b2d26baa4
objektwerks <objektwerks@runbox.com>
2025-05-21 14:07:57 -0400

    added survey validator

--- src/main/scala/objektwerks/Validators.scala
+++ src/main/scala/objektwerks/Validators.scala
@@ -17,3 +17,10 @@ object Validators:
         .validate(account.email.isEmail)(Field("Email"), Message("must be at least 3 characters in length and contain 1 @ symbol."))
         .validate(account.pin.isPin)(Field("Pin"), Message("must be 7 characters in length."))
         .validate(account.activated.nonEmpty)(Field("Activated"), Message("must be non empty."))
+
+  extension (survey: Survey)
+    def validate: Validator =
+      Validator()
+        .validate(survey.id >= 0)(Field("Id"), Message("must be greater than or equal to 0."))
+        .validate(survey.accountId > 0)(Field("AccountId"), Message("must be greater than 0."))
+        .validate(survey.created.nonEmpty)(Field("Built"), Message("must be non empty."))
