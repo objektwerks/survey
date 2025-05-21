d8027e507c8b5e3e579ae7c4394950319fa03d7d
objektwerks <objektwerks@runbox.com>
2025-05-21 13:30:14 -0400

    added emailer

---
+++ src/main/scala/objektwerks/Emailer.scala
@@ -0,0 +1,47 @@
+package objektwerks
+
+import com.typesafe.config.Config
+import com.typesafe.scalalogging.LazyLogging
+
+import jodd.mail.{Email, MailServer, SmtpServer}
+
+import scala.language.postfixOps
+import scala.util.Using
+
+object Emailer:
+  def apply(config: Config): Emailer =
+    val host = config.getString("email.host")
+    val sender = config.getString("email.sender")
+    val password = config.getString("email.password")
+    val subject = config.getString("email.subject")
+    new Emailer(host, sender, password, subject)
+
+private final class Emailer(host: String,
+                            sender: String,
+                            password: String,
+                            subject: String) extends LazyLogging:
+  private val smtpServer: SmtpServer = MailServer.create
+    .host(host)
+    .ssl(true)
+    .auth(sender, password)
+    .buildSmtpMailServer
+
+  private def sendEmail(recipients: List[String],
+                        message: String): Unit =
+    Using( smtpServer.createSession ) { session =>
+      val email = Email.create
+        .from(sender)
+        .subject(subject)
+        .textMessage(message, "UTF-8")
+        .cc(sender)
+      recipients.foreach( recipient => email.to(recipient) )
+      session.open()
+      val messageId = session.sendMail(email)
+      logger.info("*** Emailer subject: {} to: {} message id: {}", subject, recipients.mkString, messageId)
+    }.recover { error =>
+      logger.error("*** Emailer subject: {} to: {} failed: {}",  subject, recipients.mkString, error.getMessage)
+      throw error
+    }
+
+  def send(recipients: List[String],
+           message: String): Unit = sendEmail(recipients, message)
