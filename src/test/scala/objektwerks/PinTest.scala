901ced113c4dea2f2f265f56c100e4ab8c6a8ee3
objektwerks <objektwerks@runbox.com>
2025-05-23 10:59:48 -0400

    added pin test

---
+++ src/test/scala/objektwerks/PinTest.scala
@@ -0,0 +1,8 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+final class PinTest extends AnyFunSuite with Matchers:
+  test("pin"):
+    for _ <- 1 to 1000 yield Pin.newInstance.length shouldBe 7
