ab6deca898c7546e39d86f42f163919816dfc779
objektwerks <objektwerks@runbox.com>
2025-05-23 15:29:21 -0400

    added dispatcher test

---
+++ src/test/scala/objektwerks/DispatcherTest.scala
@@ -0,0 +1,18 @@
+package objektwerks
+
+import com.typesafe.config.ConfigFactory
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import scala.sys.process.Process
+
+final class DispatcherTest extends AnyFunSuite with Matchers:
+  val exitCode = Process("psql -d survey -f ddl.sql").run().exitValue()
+  exitCode shouldBe 0
+
+  val config = ConfigFactory.load("test.conf")
+  val store = Store(config)
+  val emailer = Emailer(config)
+  val handler = Handler(store, emailer)
+  val dispatcher = Dispatcher(handler)
