8bbb7980f8605012433f5aed86194a047bcac077
objektwerks <objektwerks@runbox.com>
2025-05-23 13:03:49 -0400

    enhanced schemas

--- src/main/scala/objektwerks/Schemas.scala
+++ src/main/scala/objektwerks/Schemas.scala
@@ -6,6 +6,7 @@ object Schemas:
   given Schema[Command] = Schema.derived
   given Schema[Event] = Schema.derived
   given Schema[Entity] = Schema.derived
+  given Schema[Participant] = Schema.derived
   given Schema[Account] = Schema.derived
   given Schema[Survey] = Schema.derived
   given Schema[Question] = Schema.derived
