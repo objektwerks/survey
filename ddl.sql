58fe6a6a55e2c70b5c00a4982a15e4bfd0467618
objektwerks <objektwerks@runbox.com>
2025-05-23 10:17:14 -0400

    enhanced ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -37,3 +37,10 @@ CREATE TABLE texts_question (
   texts VARCHAR NOT NULL,
   created VARCHAR(10) NOT NULL
 );
+
+CREATE TABLE text_answer (
+  question_id BIGINT REFERENCES text_question(id) | texts_question(id),
+  partipant_id BIGINT REFERENCES survey(id),
+  question VARCHAR NOT NULL,
+  created VARCHAR(10) NOT NULL
+);
