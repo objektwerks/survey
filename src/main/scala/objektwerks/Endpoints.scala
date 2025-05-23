99b33b269df20a2894e208984aed101036adfa7d
objektwerks <objektwerks@runbox.com>
2025-05-23 10:54:42 -0400

    added endpoints

---
+++ src/main/scala/objektwerks/Endpoints.scala
@@ -0,0 +1,31 @@
+package objektwerks
+
+import com.typesafe.scalalogging.Logger
+
+import sttp.shared.Identity
+import sttp.tapir.*
+import sttp.tapir.json.jsoniter.*
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
+
+import Serializers.given
+import Schemas.given
+
+final class Endpoints(path: String,
+                      dispatcher: Dispatcher,
+                      logger: Logger):
+  val commandEndpoint =
+    endpoint
+      .post
+      .in(path)
+      .in(jsonBody[Command])
+      .out(jsonBody[Event])
+      .handleSuccess { command =>
+        logger.info(s"*** command: $command")
+        val event = dispatcher.dispatch(command)
+        logger.info(s"*** event: $event")
+        event
+      }
+
+  val swaggerEndpoints =
+    SwaggerInterpreter()
+      .fromServerEndpoints[Identity](List(commandEndpoint), "Survey", "1.0")
