e9a02872b4de82ba19df6d75382a8bbe361c911e
objektwerks <objektwerks@runbox.com>
2025-05-23 10:37:06 -0400

    enhanced handler

--- src/main/scala/objektwerks/Handler.scala
+++ src/main/scala/objektwerks/Handler.scala
@@ -24,3 +24,14 @@ final class Handler(store: Store, emailer: Emailer):
   def sendEmail(email: String, message: String): Unit =
     val recipients = List(email)
     emailer.send(recipients, message)
+
+  def register(email: String): Event =
+    try
+      supervised:
+        val account = Account(email = email)
+        val message = s"Your new pin is: ${account.pin}\n\nWelcome aboard!"
+        retry( RetryConfig.delay(1, 600.millis) )( sendEmail(account.email, message) )
+        val id = store.register(account)
+        Registered( account.copy(id = id) )
+    catch
+      case NonFatal(error) => addFault( Fault(s"Registration failed for: $email, because: ${error.getMessage}") )
