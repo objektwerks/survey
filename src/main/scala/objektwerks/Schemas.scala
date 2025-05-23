ec3a6696244117653e6711b2be53cbfc1994b5fc
objektwerks <objektwerks@runbox.com>
2025-05-23 10:52:41 -0400

    added schemas

---
+++ src/main/scala/objektwerks/Schemas.scala
@@ -0,0 +1,13 @@
+package objektwerks
+
+import sttp.tapir.Schema
+
+object Schemas:
+  given Schema[Command] = Schema.derived
+  given Schema[Event] = Schema.derived
+  given Schema[Entity] = Schema.derived
+  given Schema[Account] = Schema.derived
+  given Schema[Survey] = Schema.derived
+  given Schema[Question] = Schema.derived
+  given Schema[Answer] = Schema.derived
+  given Schema[Fault] = Schema.derived
