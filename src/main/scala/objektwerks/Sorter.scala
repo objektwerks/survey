03cf9e8c958c9e482e2a885fa1a8c504b3e91d37
objektwerks <objektwerks@runbox.com>
2025-05-23 09:47:55 -0400

    enhanced sorter

--- src/main/scala/objektwerks/Sorter.scala
+++ src/main/scala/objektwerks/Sorter.scala
@@ -4,3 +4,4 @@ object Sorter:
   given Ordering[Account] = Ordering.by[Account, String](account => account.activated).reverse
   given Ordering[Survey] = Ordering.by[Survey, String](survey => survey.released).reverse
   given Ordering[Question] = Ordering.by[Question, String](question => question.created).reverse
+  given Ordering[Answer] = Ordering.by[Answer, String](answer => answer.answered).reverse
