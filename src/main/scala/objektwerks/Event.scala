599e835522a9a47b70c292cb55675cc083ae9647
objektwerks <objektwerks@runbox.com>
2025-05-23 09:59:57 -0400

    added fault to event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -5,3 +5,9 @@ sealed trait Event
 final case class Registered(account: Account) extends Event
 
 final case class LoggedIn(account: Account) extends Event
+
+final case class Fault(cause: String, occurred: String = Entity.now) extends Event
+
+final case class FaultsListed(faults: List[Fault]) extends Event
+
+final case class FaultAdded(fault: Fault) extends Event
