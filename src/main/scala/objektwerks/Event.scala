58ba328636082a14da7e0216bd01c45344e38b54
objektwerks <objektwerks@runbox.com>
2025-05-25 12:49:54 -0400

    added survey events

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -10,6 +10,12 @@ final case class ParticipantListed(participant: Participant) extends Event
 
 final case class ParticipantAdded(id: Long) extends Event
 
+final case class SurveysListed(surveys: List[Survey]) extends Event
+
+final case class SurveyAdded(id: Long) extends Event
+
+final case class SurveyUpdated(count: Int) extends Event
+
 final case class Fault(cause: String, occurred: String = Entity.now) extends Event
 
 final case class FaultsListed(faults: List[Fault]) extends Event
