e1107497e40579c66881cb211780d9a8c710b7e7
objektwerks <objektwerks@runbox.com>
2025-05-25 12:45:44 -0400

    added participant listed event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -6,6 +6,8 @@ final case class Registered(account: Account) extends Event
 
 final case class LoggedIn(account: Account) extends Event
 
+final case class ParticipantListed(participant: Participant) extends Event
+
 final case class Fault(cause: String, occurred: String = Entity.now) extends Event
 
 final case class FaultsListed(faults: List[Fault]) extends Event
