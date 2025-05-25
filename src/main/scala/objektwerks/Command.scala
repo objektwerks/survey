c722846a94fc7f88b64974ddb4379a9a44c0c387
objektwerks <objektwerks@runbox.com>
2025-05-25 11:49:53 -0400

    enhanced command

--- src/main/scala/objektwerks/Command.scala
+++ src/main/scala/objektwerks/Command.scala
@@ -9,6 +9,9 @@ final case class Register(email: String) extends Command
 
 final case class Login(email: String, pin: String) extends Command
 
+final case class ListSurveys(license: String, accountId: Long) extends Command with License
+
+
 final case class ListFaults(license: String) extends Command with License
 
 final case class AddFault(license: String, fault: String) extends Command with License
