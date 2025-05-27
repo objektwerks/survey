8a00936b52e5c67e129d1a47e4249f50991306bc
objektwerks <objektwerks@runbox.com>
2025-05-27 16:51:09 -0400

    enhanced dispatcher

--- src/main/scala/objektwerks/Dispatcher.scala
+++ src/main/scala/objektwerks/Dispatcher.scala
@@ -16,7 +16,7 @@ final class Dispatcher(handler: Handler):
               case Login(email, pin)                       => handler.login(email, pin)
               case ListParticipant(_, email)               => handler.listParticipant(email)
               case AddParticipant(_, participant)          => handler.addParticipant(participant)
-              case ListSurveys(_, accountId)               => Fault("TODO")
+              case ListSurveys(_, accountId)               => handler.listSurveys(accountId)
               case AddSurvey(_, survey)                    => Fault("TODO")
               case UpdateSurvey(_, survey)                 => Fault("TODO")
               case ListQuestions(_, surveyId)              => Fault("TODO")
